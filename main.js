b86be3ca672c0413cfbdc614c6941160607ccadb
jini0012 <jinhui.ha1208@gmail.com>
2024-12-31 17:10:10 +0900

    ♻️ 코드 리팩토링 및 클래스명 일괄 수정

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-const dialog = document.getElementById("subscribe");
-const showDialogBtn = document.getElementById("showDialogBtn");
-const closeBtn = document.getElementById("closeBtn");
+const subscribeDialog = document.querySelector(".subscribe-dialog");
+const showDialogBtn = document.querySelector(".show-dialog-btn");
+const closeBtn = document.querySelector(".close-dialog-btn");
 
-showDialogBtn.addEventListener("click", () => {
-  dialog.showModal();
+showDialogBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const scrollY = window.scrollY;
+  subscribeDialog.showModal();
+
   document.body.style.position = "fixed";
   document.body.style.top = `-${scrollY}px`;
 });
 
 closeBtn.addEventListener("click", () => {
-  dialog.close();
   const scrollY = document.body.style.top;
   document.body.style.position = "";
   document.body.style.top = "";
   window.scrollTo(0, parseInt(scrollY || "0") * -1);
 
+  subscribeDialog.close();
 });
 
-const navBtn = document.querySelector(".nav-btn");
+const navBtn = document.querySelector(".mobile-nav-btn");
 const headerNav = document.querySelector("header nav");
 
 navBtn.addEventListener("click", () => {
